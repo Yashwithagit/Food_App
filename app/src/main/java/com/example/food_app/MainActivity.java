fa12fa24a4dd3cd3b667dfcb6e9aa86f2d1c573c
Yashwithagit <yashwithakrishnappa12@gmail.com>
2021-07-31 15:44:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/food_app/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.food_app;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button login;
+Button register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login=(Button)findViewById(R.id.login);
+        register=(Button)findViewById(R.id.reg);
+
+        //Transparent ActionBar
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+        getSupportActionBar().hide();
+
+    }
+}
